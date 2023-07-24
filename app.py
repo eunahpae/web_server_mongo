15f5a2b49f75f3db291c9f942da3b59b7f6e0dfa
EunAh <eunahp86@gmail.com>
2023-07-24 18:08:56 +0900

    mongoDB

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,session
 from data import Articles
 from models import MyMongo
 from config import MONGODB_URL
+from datetime import timedelta
+from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = 'eunah'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+mymongo = MyMongo(MONGODB_URL, 'os')
 
-mymango = MyMongo(MONGODB_URL, 'os')
+def is_logged(func):
+    @wraps(func)
+    def wrap(*args,**kwargs):
+        if 'is_logged' in session:
+            return func(*args,**kwargs)
+        else:
+            return redirect('/login')
+    return wrap
+
+def is_admin(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if session['username'] == "admin":
+            return f(*args,**kwargs)
+        else:
+            return redirect('/')
+    return wrap
 
 @app.route('/', methods=['GET','POST'])
+@is_logged
 def index():
-    data = Articles()
-    return render_template('index.html', data = data)
+    return render_template('index.html')
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -19,11 +42,86 @@ def register():
         email = request.form['email']
         phone = request.form['phone']
         password = request.form['password']
-        print(type(password))      
-        result =  mymango.user_insert(username,email,phone,password)
-        
-        return str(result)      
+        user = mymongo.find_user(email)
+        if username == 'admin':
+            return render_template('register.html', message ='Impossible')
+                # print(type(password))      
+        elif user:            
+            return render_template('register.html', message ='X')
+        else:
+            result =  mymongo.user_insert(username,email,phone,password)
+            return redirect('/login')  
+    
+    elif request.method == 'GET':
+        return render_template('register.html')    
+    
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        result = mymongo.verify_password(email,password)
+        user = mymongo.find_user(email)
+        if result == "1":
+            session['is_logged'] = True
+            session['username'] = user['username']
+            return render_template('index.html', message = user )
+        elif result == "2":
+            return render_template('login.html', message = 'Wrong' )
+        else:
+            return render_template('register.html', message = 'None')
+    print(result)
+    return result
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/edit')
+def edit():
+    edit = mymongo.edit()
+    return redirect('/')
+
+@app.route('/delete')
+def delete():
+    delete = mymongo.delete()
+    return redirect('/')
+
+@app.route('/admin', methods=['GET','POST'])
+@is_logged
+@is_admin
+def admin():
+    return render_template('admin.html')
+
+
+
+@app.route('/list', methods=['GET','POST'])
+@is_logged
+def list():
+    data = mymongo.find_data()
+    # for i in data:
+    #     print(i)
+    return render_template('list.html', data=data)
+
+@app.route('/create', methods=['GET','POST'])
+@is_logged
+def create():  
+    if request.method == 'GET':
+        return render_template('create.html')
+    else:
+        title = request.form['title']
+        desc = request.form['desc']  
+        author = request.form['author']
+        # print(author) 
+        # return 'a'    
+        result = mymongo.insert_data(title,desc,author) 
+        return redirect('/list')
+
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=9999)
+    
--- models.py
+++ models.py
@@ -39,23 +39,79 @@ class MyMongo:
         print(result)
         return 1
         
-    def verify_password(self,input_password, id): 
+    def verify_password(self, email, input_password): 
         db = self.client.os
         users = db.users      
-        user = users.find_one({'_id':id}) 
+        user = users.find_one({'email':email}) 
+        # print(user)
         if user:
             result = check_password(input_password,user['password'])
             if result:
                 print('Verify Success')
+                return "1"
             else:
                 print("Verify Fail")
+                return "2"
         else:
-            print("ID is not founded.")        
+            print("ID is not founded.")
+            return "3"
+            
+    def find_user(self, email):
+        db = self.client.os
+        users = db.users      
+        user = users.find_one({'email':email}) 
+        # print(user)
+        return user
+    
+    def find_data(self):
+        db = self.client.os
+        lists = db.lists      
+        list = lists.find()
+        # for i in list:
+        #     print(i)
+        return list
+    
+    def insert_data(self, title, desc, author):
+        db = self.client.os
+        lists = db.lists
+        data = {
+            'title':title,
+            'desc':desc,
+            'author':author,
+            'create_at':datetime.datetime.utcnow()
+        }      
+        list = lists.insert_one(data)
+        # for i in list:
+        #     print(i)
+        return list
+    
+    def edit_data(self, title, desc, author):
+        db = self.client.os
+        lists = db.lists
+        data = {
+            'title':title,
+            'desc':desc,
+            'author':author,
+            'create_at':datetime.datetime.utcnow()
+        }      
+        list = lists.insert_one(data)
+        # for i in list:
+        #     print(i)
+        return list
+    
+    def delete_data(self, title):
+        db = self.client.os
+        lists = db.lists
+        data = self.find_data(title)    
+        delete = lists.deleteOne(data)
+        # for i in list:
+        #     print(i)             
     
 
-mymango = MyMongo(MONGODB_URL, 'os')
-mymango.verify_password("1234", ObjectId('64ba26436b5bac3f54684a45'))    
-        
+# mymongo = MyMongo(MONGODB_URL, 'os')
+# # mymongo.verify_password('1@naver.com','1234')    
+# # mymongo.find_user('1@naver.com','1234')
+# mymongo.find_data()
 
 # mongodb_URI = MONGODB_URL
 # client = MongoClient(mongodb_URI)
